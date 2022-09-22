de39d681ce4358ab0c8120e598d18f6e3ea58bba
williamjordan <will.jordan0788@gmail.com>
2022-09-22 10:49:54 -0400

    commit tree 4f6555a290132255e9815c0913cbc1c3e7781b8f

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    def name=(leroy)
+        @name = leroy
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(pitbull)
+        @breed = pitbull
+    end
+
+    def breed
+        @breed
+    end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+    def name=(Alex)
+        @name = Alex
+    end
+
+    def name
+        @name
+    end
+
+    def job=(teacher)
+        @job = teacher
+    end
+
+    def job
+        @job
+    end
 
 end
